d7d673ba296f65750eb4ac90b3b428796b757fc0
conmeno <conmeno@outlook.com>
2016-03-30 16:09:12 +0700

    fff

--- SwiftSnake/AdManager/Data.swift
+++ SwiftSnake/AdManager/Data.swift
@@ -10,14 +10,19 @@ import Foundation
 
 class Data {
     
-    //Snake 1998 game
+    //Snake 1999 game
+    
+//    Ad unit name: snake banner
+//    Ad unit ID: ca-app-pub-3457738497134865/3428234430
+//    Ad unit name: Snake Full
+//    Ad unit ID: ca-app-pub-3457738497134865/4904967635
     
     //URL setup online
     let AdURL: String = "http://www.tony.somee.com/snake1999-v2.txt"
     
     //google
-    let gBanner : String = ""
-    let gFull : String = ""
+    let gBanner : String = "ca-app-pub-3457738497134865/3428234430"
+    let gFull : String = "ca-app-pub-3457738497134865/4904967635"
     let TestDeviceID: String = ""
     
     
@@ -25,11 +30,11 @@ class Data {
     let cAppID: String = ""
     let cSign : String = ""
     //amazon
-    let AmazonKey: String = ""
+    let AmazonKey: String = "729491e1690143fdaed868d15f3b7bbf"
     
     //adcolony
-    let AdcolonyAppID: String = ""
-    let AdcolonyZoneID: String = ""
+    let AdcolonyAppID: String = "appf3d05360d62e4ceda0"
+    let AdcolonyZoneID: String = "vz57e65dce2e05400ea4"
     
     //revmob appID
     let RevmobID: String = ""
@@ -37,8 +42,8 @@ class Data {
     //Vungle ID
     let VungleID: String = ""
     //startapp
-    let StartAppID: String = "202174021"
-    let StartAppAccountID: String = " 102803784"
+    let StartAppID: String = "202190885"
+    let StartAppAccountID: String = "102206946"
     
     
     
