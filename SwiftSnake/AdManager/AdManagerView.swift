ee7df5a51b343e92282289778d03111240bf59c3
conmeno <conmeno@outlook.com>
2016-03-31 14:33:07 +0700

    ff

--- SwiftSnake/AdManager/AdManagerView.swift
+++ SwiftSnake/AdManager/AdManagerView.swift
@@ -15,14 +15,15 @@ class AdManagerView: UIViewController
 {
     
     
-    //    var isAd1 = true//admob full
-    //    var isAd2 = false//charbootst
-    //    var isAd3 = false//auto chartboost
-    //    var isAd4 = true//admob banner
-    //    var isAd5 = false//adcolony
-    //    var isAd6 = true//amazon
-    //    var isAd7 = true//Admob Edit
-    //    var isAd8 = true//ChartBoost Edit
+    var isAd1 = false//admob full
+    var isAd2 = true//admob Banner
+    var isAd3 = false//amazon
+    var isAd4 = false//adcolony
+    var isAd5 = false//startapp
+         var CheckOnline = true
+    
+      
+    var showOtherAd1 = false //showAd (ngoai tru Admob Banner)
     
     @IBOutlet weak var sw1: UISwitch!
     
@@ -31,6 +32,8 @@ class AdManagerView: UIViewController
     @IBOutlet weak var sw3: UISwitch!
     
     @IBOutlet weak var sw4: UISwitch!
+    
+    @IBOutlet weak var sw5: UISwitch!
     
   @IBOutlet weak var ShowOtherAd: UISwitch!
     
@@ -41,36 +44,42 @@ class AdManagerView: UIViewController
     @IBAction func sw1Action(sender: UISwitch) {
         NSUserDefaults.standardUserDefaults().setObject(sender.on, forKey:"ad1")
         NSUserDefaults.standardUserDefaults().synchronize()
-        Utility.Static.isAd1 = sender.on
+       // Utility.Static.isAd1 = sender.on
         
     }
     
     @IBAction func sw2Action(sender: UISwitch) {
         NSUserDefaults.standardUserDefaults().setObject(sender.on, forKey:"ad2")
         NSUserDefaults.standardUserDefaults().synchronize()
-        Utility.Static.isAd2 = sender.on
+       // Utility.Static.isAd2 = sender.on
     }
     
     
     @IBAction func sw3Action(sender: UISwitch) {
         NSUserDefaults.standardUserDefaults().setObject(sender.on, forKey:"ad3")
         NSUserDefaults.standardUserDefaults().synchronize()
-        Utility.Static.isAd3 = sender.on
+       // Utility.Static.isAd3 = sender.on
     }
     
     
     @IBAction func sw4Action(sender: UISwitch) {
         NSUserDefaults.standardUserDefaults().setObject(sender.on, forKey:"ad4")
         NSUserDefaults.standardUserDefaults().synchronize()
-        Utility.Static.isAd4 = sender.on
+      //  Utility.Static.isAd4 = sender.on
+    }
+    
+    @IBAction func sw5Action(sender: UISwitch) {
+        NSUserDefaults.standardUserDefaults().setObject(sender.on, forKey:"ad5")
+        NSUserDefaults.standardUserDefaults().synchronize()
+        //  Utility.Static.isAd4 = sender.on
     }
     
        
     @IBAction func ShowOtherAd(sender: UISwitch) {
         NSUserDefaults.standardUserDefaults().setObject(sender.on, forKey:"show-other-ad")
         NSUserDefaults.standardUserDefaults().synchronize()
-        Utility.Static.showOtherAd = sender.on
-        if(Utility.Static.showOtherAd)
+        //Utility.Static.showOtherAd = sender.on
+        if(sender.on)
         {
             NSUserDefaults.standardUserDefaults().setObject("true", forKey:"show-other-ad-online")
             NSUserDefaults.standardUserDefaults().synchronize()
@@ -81,7 +90,7 @@ class AdManagerView: UIViewController
     @IBAction func CheckAdOnlineAction(sender: UISwitch) {
         NSUserDefaults.standardUserDefaults().setObject(sender.on, forKey:"adOnline")
         NSUserDefaults.standardUserDefaults().synchronize()
-        Utility.Static.CheckOnline = sender.on
+        //Utility.Static.CheckOnline = sender.on
     }
     
     
@@ -127,67 +136,57 @@ class AdManagerView: UIViewController
     }
     func CheckAdOptionValue()
     {
-        //        //ad1 admob
-        //        if(NSUserDefaults.standardUserDefaults().objectForKey("ad1") != nil)
-        //        {
-        //            isAd1 = NSUserDefaults.standardUserDefaults().objectForKey("ad1") as! Bool
-        //
-        //        }
-        //
-        //        //ad2 charboost
-        //
-        //        if(NSUserDefaults.standardUserDefaults().objectForKey("ad2") != nil)
-        //        {
-        //            isAd2 = NSUserDefaults.standardUserDefaults().objectForKey("ad2") as! Bool
-        //
-        //        }
-        //
-        //
-        //        //ad3 ...
-        //
-        //        if(NSUserDefaults.standardUserDefaults().objectForKey("ad3") != nil)
-        //        {
-        //            isAd3 = NSUserDefaults.standardUserDefaults().objectForKey("ad3") as! Bool
-        //
-        //        }
-        //
-        //        if(NSUserDefaults.standardUserDefaults().objectForKey("ad4") != nil)
-        //        {
-        //            isAd4 = NSUserDefaults.standardUserDefaults().objectForKey("ad4") as! Bool
-        //
-        //        }
-        //
-        //        if(NSUserDefaults.standardUserDefaults().objectForKey("ad5") != nil)
-        //        {
-        //            isAd5 = NSUserDefaults.standardUserDefaults().objectForKey("ad5") as! Bool
-        //
-        //        }
-        //
-        //        if(NSUserDefaults.standardUserDefaults().objectForKey("ad6") != nil)
-        //        {
-        //            isAd6 = NSUserDefaults.standardUserDefaults().objectForKey("ad6") as! Bool
-        //
-        //        }
-        //
-        //
-        //        if(NSUserDefaults.standardUserDefaults().objectForKey("ad7") != nil)
-        //        {
-        //            isAd7 = NSUserDefaults.standardUserDefaults().objectForKey("ad7") as! Bool
-        //
-        //        }
-        //
-        //        if(NSUserDefaults.standardUserDefaults().objectForKey("ad8") != nil)
-        //        {
-        //            isAd8 = NSUserDefaults.standardUserDefaults().objectForKey("ad8") as! Bool
-        //            
-        //        }
+        //ad1 admob
+        if(NSUserDefaults.standardUserDefaults().objectForKey("ad1") != nil)
+        {
+            isAd1 = NSUserDefaults.standardUserDefaults().objectForKey("ad1") as! Bool
+            
+        }
         
-        sw1.on = Utility.Static.isAd1
-        sw2.on = Utility.Static.isAd2
-        sw3.on = Utility.Static.isAd3
-        sw4.on = Utility.Static.isAd4
-         ShowOtherAd.on = Utility.Static.showOtherAd
-        CheckAdOnline.on = Utility.Static.CheckOnline
+        //ad2 charboost
+        
+        if(NSUserDefaults.standardUserDefaults().objectForKey("ad2") != nil)
+        {
+            isAd2 = NSUserDefaults.standardUserDefaults().objectForKey("ad2") as! Bool
+            
+        }
+        
+        
+        // ad3 ...
+        
+        if(NSUserDefaults.standardUserDefaults().objectForKey("ad3") != nil)
+        {
+            isAd3 = NSUserDefaults.standardUserDefaults().objectForKey("ad3") as! Bool
+            
+        }
+        
+        if(NSUserDefaults.standardUserDefaults().objectForKey("ad4") != nil)
+        {
+            isAd4 = NSUserDefaults.standardUserDefaults().objectForKey("ad4") as! Bool
+            
+        }
+        
+        if(NSUserDefaults.standardUserDefaults().objectForKey("ad5") != nil)
+        {
+            isAd5 = NSUserDefaults.standardUserDefaults().objectForKey("ad5") as! Bool
+            
+        }
+        
+        if(NSUserDefaults.standardUserDefaults().objectForKey("adOnline") != nil)
+        {
+            CheckOnline = NSUserDefaults.standardUserDefaults().objectForKey("adOnline") as! Bool
+            print(NSUserDefaults.standardUserDefaults().objectForKey("adOnline"))
+        }
+        
+
+        
+        sw1.on = isAd1
+        sw2.on = isAd2
+        sw3.on = isAd3
+        sw4.on = isAd4
+        sw5.on = isAd4
+        //ShowOtherAd1.on = Utility.Static.showOtherAd
+        CheckAdOnline.on = CheckOnline
         
     }
     
--- SwiftSnake/AppDelegate.swift
+++ SwiftSnake/AppDelegate.swift
@@ -5,10 +5,11 @@ import iAd
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var UIiAd: ADBannerView = ADBannerView()
 	var window: UIWindow?
+    let data = Data()
 	  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
        
-        Utility.Static.SetUpAdData()
+        //Utility.Static.SetUpAdData()
 
         
         //if(Utility.isAd3)
@@ -18,10 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //}
 
         
-        if(Utility.Static.isAd4)
-        {
+        //if(Utility.Static.isAd4)
+        //{
             AdColony.configureWithAppID(Utility.Static.AdcolonyAppID, zoneIDs: [Utility.Static.AdcolonyZoneID], delegate: nil, logging: true)
-        }
+        //}
         
         return true
 	}
